7fd6e0629c371dd3cf50aeaae611772adda07bc2
EliasLaaj <64526929+EliasLaaj@users.noreply.github.com>
2023-09-15 12:16:16 +0300

    Update main.py
    main applied with needed backend functionality

--- main.py
+++ main.py
@@ -18,14 +18,28 @@ if __name__ == '__main__':
     odo = odoread()
     trip = tripread()
     gear_speed_rpm = get_gear_speed_and_rpm()
+    scene = 1
 
 
     while True: 
-        get arvot
-        print(kaikki arvot) 
-        get arvotspeed,rpm, gear
-        print(kaikki arvot) 
-        get arvotspeed,rpm, gear
-        print(kaikki arvot)
+        status = getstatus()
+        scenereturn = scenedrawer(scene, status, odo, trip)
+        sceneout = scenereturn[0] #output string is 1. datapoint in list
+        if scene == 2:
+            trip = scenereturn[1] #if reset button have been used, scenereturn 2. datapoint is new trip(0.0)
+        print(gear_speed_rpm, status, sceneout) 
+        gear_speed_rpm = get_gear_speed_and_rpm()  # update only gear, speed and rpm data to save process time
+        print(gear_speed_rpm, status, sceneout) 
+    
+        if read_volts_12() < 8.0: #checking if power input is below voltagelimit of 8v. If true, shuts instrumentcluster down.
+	        time.sleep(2)
+            if read_volts_12() <8.0: # Checking again that power is cutted to commit shutdown
+                def shutdownwrite(odo, trip)
+                # input shutdown commands here
+                time.sleep(3)
+                os.system("shutdown now -h")
+        else:
+            time.sleep(0.05)
+        
 
  
