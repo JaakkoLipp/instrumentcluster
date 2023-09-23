a27b7342709bd881f148a2f8ac5793eeb0ff5270
Elias Laajalahti <elia.laaja@gmail.com>
2023-09-23 17:14:54 +0300

    added cleanups

--- subprograms.py
+++ subprograms.py
@@ -375,12 +375,14 @@ def scenedrawer(scene, getstatus, odo, trip, qs_status): #subprogram outputs str
                 GPIO.setmode(GPIO.BCM)
                 GPIO.setup(QS_PIN, GPIO.OUT)
                 GPIO.output(QS_PIN, GPIO.LOW)
+                GPIO.cleanup()
             else:
                 qs_status = 1
                 qs_status_string = "Quickshifter enabled."
                 GPIO.setmode(GPIO.BCM)
                 GPIO.setup(QS_PIN, GPIO.OUT)
                 GPIO.output(QS_PIN, GPIO.HIGH)
+                GPIO.cleanup()
         
         return [qs_status_string]
         
