a1862154079cdffd94c12785ee3e143b45d0616d
Jaakko <46355010+JaakkoLipp@users.noreply.github.com>
2023-11-28 16:20:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,36 @@ import RPi.GPIO as GPIO
 
 #TODO check if imports needed in subprograms
 
+CORRECTION = 1 #speedometer CORRECTION value, 1,0 is stock from factory
+GEAR_RATIO = [2.533, 2.053, 1.737, 1.524, 1.381, 1.304] # gears 1 to 6 ratios
+GEAR_SENSITIVITY = 0.10 # How much variability allowed around calculated gear ratios
+SPEEDRATIO = 0.779221 # ratio between speed and frequency (how many kmh per hz) for example( 60kmh / 77hz = 0.779221 kmh/hz)
+FRONT_SPROCKET_PULSES_PER_ROTATION = 4 # How many pulses speedsensor sends each rotation of front sprocket
+MULTIPLIER_12V = 1 # Must be defined to run code correctly!!  #TODO check multiplier value
+HIREADLIMIT = 0.6 #adc output value which is minimum for activation of hiread, used for positive voltage input
+LOWREADLIMIT = 0.25 #adc output value which is maximum for activation of lowread, used for ground sensing input
+NIGHTMODETHRESHOLD= 30 #TODO #resistance for nightmode activation threshold
+BUTTONSLEEP = 0.8 #sleeptime to detect long press
+SCENEMAX = 4 #How many changing scenes is available by scene change button
+
+#TODO gpio input pins plox check correct
+SPEEDPIN = 36 #speedometer input gpio pin
+RPM_PIN= 37 #rpm input pin
+QS_PIN = 23 #gpio output pin for quicshifter controlling, currently 1 for activated and 0 for disabled
+NEUTRAL_LIST = ["/dev/spidev1.0", 5] #neutralpin adc [device, channel 0-7]
+V12_READ_INPUTLIST = ["/dev/spidev1.0", 7] #12v sensing inputpin adc [device, channel 0-7]
+WATERTEMP_INPUT_LIST = ["/dev/spidev1.1", 0, 5] #watertemp inputpin adc [device, channel 0-7], watertemp multiplier by resistance
+RESERVEFUEL_INPUT_LIST = ["/dev/spidev1.1", 2, 5] #reservefuel inputpin adc [device, channel 0-7], reserve fuel state multiplier by resistance
+BLINKER_LEFT_LIST = ["/dev/spidev1.0", 0]
+BLINKER_RIGHT_LIST = ["/dev/spidev1.0", 1]
+HI_BEAM_LIST = ["/dev/spidev1.0", 2]
+LEFT_BUTTON_LIST = ["/dev/spidev1.0", 3]
+RIGHT_BUTTON_LIST = ["/dev/spidev1.0", 4]
+ENGINE_LIGHT_LIST = ["/dev/spidev1.0", 6]
+OIL_LIGHT_LIST = ["/dev/spidev1.1", 3]
+AMBIENT_LIGHT_LIST = ["/dev/spidev1.1", 1, 5] #ambientlight resistor inputpin adc [device, channel 0-7], ambient light multiplier by resistance
+AMBIENT_TEMP_LIST = ["/dev/spidev1.1", 4, 5]  #ambient temperature resistor inputpin adc [device, channel 0-7], ambient temp multiplier by resistance
+
 
 odo = odoread() #datatype kilometers
 trip = tripread() #datatype kilometers
