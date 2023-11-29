3aa4852373b751a2302de7fce2a123a953f069fc
Jaakko <jaakko.lipponen01@gmail.com>
2023-11-29 12:36:50 +0200

    mayby now??

--- subprograms.py
+++ subprograms.py
@@ -124,7 +124,8 @@ def getspeed(SPEEDPIN, SPEEDRATIO, CORRECTION):
             
             if GPIO.wait_for_edge(SPEEDPIN, GPIO.FALLING, timeout=260) is None:  #if timeout occures, return speed 0
                 print("exited before getspeed loop")
-                return([0, 0]) 
+                corrected_speed = 0 #setting output values for timeout
+                final_frequency = 0
             # Measure time between falling edges
             if prev_time is not None:
                 time_difference = time.time() - prev_time
