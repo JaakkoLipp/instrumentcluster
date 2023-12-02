7f7ebc167b0f19c72b503f00f0eba8165822a5ef
Jaakko <jaakko.lipponen01@gmail.com>
2023-12-02 15:03:24 +0200

    bcm on spi

--- spitest2.py
+++ spitest2.py
@@ -2,10 +2,10 @@ import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 
-CLK = 23
-MISO = 21
-MOSI = 19
-CS = 24
+CLK = 11
+MISO = 9
+MOSI = 10
+CS = 8
 
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 print('Reading MCP3008 values, press Ctrl-C to quit...')
