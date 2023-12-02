31a02f393a493c1a25dfb078b452a0ce7762537c
Jaakko <jaakko.lipponen01@gmail.com>
2023-12-02 15:00:04 +0200

    fixd spi2

--- spitest2.py
+++ spitest2.py
@@ -16,7 +16,7 @@ while True:
     values = [0]*8
     for i in range(8):
 
-    values[i] = mcp.read_adc(i)
-    print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
+        values[i] = mcp.read_adc(i)
+        print('| {0:>4} | {1:>4} | {2:>4} | {3:>4} | {4:>4} | {5:>4} | {6:>4} | {7:>4} |'.format(*values))
 
     time.sleep(0.5)
