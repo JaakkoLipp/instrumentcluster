dc5cc6b030a4a08f703e12cb58b8a8ca1be852dc
EliasLaaj <64526929+EliasLaaj@users.noreply.github.com>
2024-01-01 21:56:28 +0200

    added loop

--- thermsensortest.py
+++ thermsensortest.py
@@ -7,5 +7,6 @@ def read_ambient_temperature(): # Pin which sensor is connected, BCM
     temperature = round(data, 1)
     return temperature
 
-print(read_ambient_temperature())
-sleep(1)
+while True:
+    print(read_ambient_temperature())
+    sleep(1)
