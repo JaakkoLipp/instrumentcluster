750345097d6a72122762a2b36c12f01f87cec9e0
EliasLaaj <64526929+EliasLaaj@users.noreply.github.com>
2024-01-01 21:54:03 +0200

    made thermtester

---
+++ thermsensortest.py
@@ -0,0 +1,11 @@
+from w1thermsensor import W1ThermSensor
+import sleep
+
+def read_ambient_temperature(): # Pin which sensor is connected, BCM
+    sensor = W1ThermSensor()
+    data = sensor.get_temperature()
+    temperature = round(data, 1)
+    return temperature
+
+print(read_ambient_temperature())
+sleep(1)
