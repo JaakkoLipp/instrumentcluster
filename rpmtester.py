249db3a76f6e2d3b7eae05d0ab9a1ac9cc122ce2
EliasLaaj <64526929+EliasLaaj@users.noreply.github.com>
2024-01-01 22:18:06 +0200

    added troubleshooting prints

--- rpmtester.py
+++ rpmtester.py
@@ -13,17 +13,21 @@ def getrpm(RPM_PIN):
     prev_time = None
     frequencies = []
     num_samples = 2
+    print("initialized")
 
     try:
         for x in range(num_samples):
+            print("waiting for first edge")
             # Wait for the first falling edge
             if GPIO.wait_for_edge(RPM_PIN, GPIO.FALLING, timeout=6000) is None:  # If timeout occures, returns rpm 0. Times out if rpm is lower than 500
                 rpm = 0
                 GPIO.cleanup()
+                print("timeout and exit")
                 return rpm 
             
             # Measure time between falling edges
             if prev_time is not None:
+                print("first pulse gotten")
                 current_time = time.time()
                 time_difference = current_time - prev_time
                 frequency = 1.0 / time_difference  # Calculate frequency
@@ -33,14 +37,16 @@ def getrpm(RPM_PIN):
                 
                 # Exit after a specified number of samples
                 if falling_edges >= num_samples:
+                    print("all pulses gotten")
                     final_frequency = sum(frequencies) / len(frequencies)
                     rpm = final_frequency * 30  # Converting frequency to rpm (6000rpm = 200hz and 30rpm = 1hz)
             prev_time = time.time()
     except Exception: 
         pass
     GPIO.cleanup()
+    print("returning calculated rpm")
     return rpm
 
 while True:
     print(getrpm(2))
-    sleep(1)
+    sleep(5)
