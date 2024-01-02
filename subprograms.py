6ef197cb1bd667b5d88b72129befc988b58e933a
EliasLaaj <64526929+EliasLaaj@users.noreply.github.com>
2024-01-02 21:09:28 +0200

    added decimal to watertemp

--- subprograms.py
+++ subprograms.py
@@ -279,8 +279,8 @@ def get_status(BLINKER_LEFT_PIN, BLINKER_RIGHT_PIN,HI_BEAM_PIN, LEFT_BUTTON_LIST
 def otherdataread(AMBIENT_LIGHT_LIST, NIGHTMODETHRESHOLD, WATERTEMP_INPUT_LIST, RESERVEFUEL_INPUT_LIST): # Outputs list containing [nightmode 1/0, reservefuelstate 1/0, watertemperature string]
 	nightmode = read_ambient_light(AMBIENT_LIGHT_LIST, NIGHTMODETHRESHOLD)           # Reads ambient light status, True or False
 	reservefuelstate = read_reservefuelstate(RESERVEFUEL_INPUT_LIST)                 # Reads reservefuel state, True or False
-	watertempint = read_watertemperature(WATERTEMP_INPUT_LIST)                       # Reada watertemperature
-	watertemprounded = round(watertempint, 0)                                        # Rounds watertemperature to full number
+	watertempint = read_watertemperature(WATERTEMP_INPUT_LIST)                       # Reads watertemperature
+	watertemprounded = round(watertempint, 1)                                        # Rounds watertemperature to full number
 	watertempstr = str(watertemprounded) + " c°"                                     # Makes watertemperature string and adds unit
 	outputlist = [nightmode, reservefuelstate, watertempstr]                         # Makes output list
 	return outputlist
